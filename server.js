c7a7ebfa2f05fa634949441c14094d0cdf219b3b
Musa Dango <musaddangoWgmail.com>
2023-09-14 22:44:47 +0100

    changed the cors url to render.com front end url - facecrypt

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use(cors({
-    origin: "http://localhost:3001",
+    origin: "https://facecrypt.onrender.com",
     xContentTypeOptions: 'nosniff',
     optionsSuccessStatus: 200,
     headers: {
