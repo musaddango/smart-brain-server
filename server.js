4f473815a548a8402a5c5008aeca003f45ae0b16
Musa Dango <musaddangoWgmail.com>
2023-09-15 09:48:27 +0100

    adjustment

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.get('/profile/:id', (req, res)=> profile.handleProfile(req, res, dbase));
 
 app.put('/image', (req, res) => image.handleImage(req, res, dbase));
 
-app.post('/imageUrl', (req, res) => image.handleImageUrl(req, res));
+app.put('/imageUrl', (req, res) => image.handleImageUrl(req, res));
 
 
 app.listen(process.env.PORT, ()=>{
